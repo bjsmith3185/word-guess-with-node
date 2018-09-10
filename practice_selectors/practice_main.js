6e4708539f59a9c6683a585de67b96f347a4da81
bjsmith3185 <bjsmith3185@hotmail.com>
2018-09-10 18:15:37 -0400

    included a check to make sure user pick a letter

--- practice_selectors/practice_main.js
+++ practice_selectors/practice_main.js
@@ -13,85 +13,174 @@ var game = {
     losses: 0,
     turns: 9,
     pickWord: function () {
-           var newWord = new Word();
-           this.word = newWord.randomWord();
-           this.singleLettersArray = newWord.lettersIntoArray();
-           this.displayArray = newWord.createLetterArray(this.singleLettersArray);
-            newWord.displayBlankWord();
+        var newWord = new Word();
+        this.word = newWord.randomWord();
+        this.singleLettersArray = newWord.lettersIntoArray();
+        this.displayArray = newWord.createLetterArray(this.singleLettersArray);
+        newWord.displayBlankWord();
     },
 
     checkLetter: function (x) {
-        console.log("1111111111111111")
-        console.log(`this is display array ${this.displayArray}`)
-        var hasAlreadyBeenGuessed = false;
-        for (var s = 0; s < this.guessedLettersArray.length; s++) {
-            if (x === this.guessedLettersArray[s]) {
-                console.log("you have already guessed " + x);
-                hasAlreadyBeenGuessed = true;
+        var isALetter = false;
+
+        var letterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        
+        for (var a = 0; a < letterArray.length; a++) {
+            if (x === letterArray[a]) {
+                 isALetter = true;
             };
         };
 
-        if (!hasAlreadyBeenGuessed) {
-            this.guessedLettersArray.push(x);
-            var letterIndex;
-            var isInWord = false;
+        if (isALetter) {
+            // guessed a letter correctly
+            var hasAlreadyBeenGuessed = false;
 
-            for (var k = 0; k < this.singleLettersArray.length; k++) {
-                if (x === this.singleLettersArray[k]) {
-                    isInWord = true;
-                    letterIndex = k;
-                    var letter = new Letter(x, true);
-                    this.displayArray[letterIndex] = letter.typeDisplayed();
+            for (var s = 0; s < this.guessedLettersArray.length; s++) {
+                if (x === this.guessedLettersArray[s]) {
+                    console.log("you have already guessed " + x);
+                    hasAlreadyBeenGuessed = true;
                 };
             };
 
-            var display1 = this.displayArray.join(" ");
-            console.log(chalk.red(`       
-                ${display1}
-                                        `));
-         
+            if (!hasAlreadyBeenGuessed) {
+                this.guessedLettersArray.push(x);
+                var letterIndex;
+                var isInWord = false;
 
-            if (!isInWord) {
-             
-                if (this.turns === 0) {
-                    this.losses++;
-                    console.log("You Lost, Maybe Try Harder Next Time....");
-                    game.displayScore();
-
-                    inquirer.prompt([
-                        {
-                            type: "confirm",
-                            message: "Do you want to play again?",
-                            name: "playAgain"
-                        },
-                    ])
-                        .then(function (again) {
-                            if (again.playAgain) {
-                                console.log("Get ready to play!");
-                                game.anotherWord();
-                            } else {
-                                console.log("Thanks for playing.")
-                                game.displayScore();
-                                return;
-                            };
-                        });
-
+                for (var k = 0; k < this.singleLettersArray.length; k++) {
+                    if (x === this.singleLettersArray[k]) {
+                        isInWord = true;
+                        letterIndex = k;
+                        var letter = new Letter(x, true);
+                        this.displayArray[letterIndex] = letter.typeDisplayed();
+                    };
                 };
 
-                this.turns--;
-                console.log(x + " is not in the word.");
-                console.log(this.turns + " guesses remaining.");
-                hasAlreadyBeenGuessed = false;
-                game.userInputLetter();
+                var display1 = this.displayArray.join(" ");
+                console.log(chalk.red(`       
+                ${display1}
+                                        `));
+
+
+                if (!isInWord) {
+
+                    if (this.turns === 0) {
+                        this.losses++;
+                        console.log("You Lost, Maybe Try Harder Next Time....");
+                        game.displayScore();
+
+                        inquirer.prompt([
+                            {
+                                type: "confirm",
+                                message: "Do you want to play again?",
+                                name: "playAgain"
+                            },
+                        ])
+                            .then(function (again) {
+                                if (again.playAgain) {
+                                    console.log("Get ready to play!");
+                                    game.anotherWord();
+                                } else {
+                                    console.log("Thanks for playing.")
+                                    game.displayScore();
+                                    return;
+                                };
+                            });
+
+                    };
+
+                    this.turns--;
+                    console.log(x + " is not in the word.");
+                    console.log(this.turns + " guesses remaining.");
+                    hasAlreadyBeenGuessed = false;
+                    game.userInputLetter();
+
+                } else {
+                    game.checkAnswer();
+                };
 
             } else {
-                game.checkAnswer();
+                hasAlreadyBeenGuessed = false;
+                game.userInputLetter();
             };
 
         } else {
-            hasAlreadyBeenGuessed = false;
+            // did not guess a real letter
+            console.log("you need to guess a letter between a - z");
             game.userInputLetter();
-        };
+        }
+
+        // var hasAlreadyBeenGuessed = false;
+
+
+        // for (var s = 0; s < this.guessedLettersArray.length; s++) {
+        //     if (x === this.guessedLettersArray[s]) {
+        //         console.log("you have already guessed " + x);
+        //         hasAlreadyBeenGuessed = true;
+        //     };
+        // };
+
+        // if (!hasAlreadyBeenGuessed) {
+        //     this.guessedLettersArray.push(x);
+        //     var letterIndex;
+        //     var isInWord = false;
+
+        //     for (var k = 0; k < this.singleLettersArray.length; k++) {
+        //         if (x === this.singleLettersArray[k]) {
+        //             isInWord = true;
+        //             letterIndex = k;
+        //             var letter = new Letter(x, true);
+        //             this.displayArray[letterIndex] = letter.typeDisplayed();
+        //         };
+        //     };
+
+        //     var display1 = this.displayArray.join(" ");
+        //     console.log(chalk.red(`       
+        //         ${display1}
+        //                                 `));
+
+
+        //     if (!isInWord) {
+
+        //         if (this.turns === 0) {
+        //             this.losses++;
+        //             console.log("You Lost, Maybe Try Harder Next Time....");
+        //             game.displayScore();
+
+        //             inquirer.prompt([
+        //                 {
+        //                     type: "confirm",
+        //                     message: "Do you want to play again?",
+        //                     name: "playAgain"
+        //                 },
+        //             ])
+        //                 .then(function (again) {
+        //                     if (again.playAgain) {
+        //                         console.log("Get ready to play!");
+        //                         game.anotherWord();
+        //                     } else {
+        //                         console.log("Thanks for playing.")
+        //                         game.displayScore();
+        //                         return;
+        //                     };
+        //                 });
+
+        //         };
+
+        //         this.turns--;
+        //         console.log(x + " is not in the word.");
+        //         console.log(this.turns + " guesses remaining.");
+        //         hasAlreadyBeenGuessed = false;
+        //         game.userInputLetter();
+
+        //     } else {
+        //         game.checkAnswer();
+        //     };
+
+        // } else {
+        //     hasAlreadyBeenGuessed = false;
+        //     game.userInputLetter();
+        // };
 
     },
 
@@ -169,7 +258,7 @@ inquirer
         },
     ])
     .then(function (gameStart) {
-        
+
         if (gameStart.confirm) {
             // start game
             console.log(chalk.yellow("Get ready to Play!"));
